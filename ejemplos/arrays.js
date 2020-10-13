1ef9458964a7089ba11e18071457ebd7ad7576e2
Viorel Rosu <viorel.rosu@gmail.com>
2020-10-13 12:52:34 +0200

    091020 - ordenar arrays

---
+++ ejemplos/arrays.js
@@ -0,0 +1,13 @@
+numeros = [];
+numeros[indice] = valor;
+numeros.length; // cantidad de elemntos que tiene el array
+
+for(var i=0; i<=numeros.length-1; i++){
+
+}
+
+for(var i=0; i<numeros.length; i++){
+
+}
+
+numeros[numeros.length] =  valor;
