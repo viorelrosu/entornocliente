6b099de78aaced9fcf31ebc0bd41379801fb6bc1
Viorel Rosu <viorel.rosu@gmail.com>
2020-11-12 12:37:19 +0100

    121110 crud numeros

--- ejercicios/ej35-ArraysNumeros/scripts.js
+++ ejercicios/ej35-ArraysNumeros/scripts.js
@@ -1,15 +1,19 @@
-var numeros = new Array(9,5,3,7,1);
+var numeros = new Array(9,53,5,24,1,7);
+var numOrdAsc = new Array();
+var numOrdDesc = new Array();
 var index = 0;
-
+var myWindow;
 var inputNum = document.getElementById('inputNum');
 var btnAceptar = document.getElementById('btnAceptar');
 var btnCancel = document.getElementById('btnCancel');
 var alertError = document.getElementById('idAlertError');
 var alertSuccess = document.getElementById('idAlertSuccess');
 var alertWarning = document.getElementById('idAlertWarning');
+var alertInfo = document.getElementById('idAlertInfo');
 var divListaOrdenadaAsc = document.getElementById('divListaOrdenadaAsc');
 var divListaOrdenadaDesc = document.getElementById('divListaOrdenadaDesc');
 var divList = document.getElementById('divList');
+var divListNumeros = document.getElementById('divListNumeros');
 var accion = '';
 
 var elements = new Array(
@@ -25,9 +29,9 @@ var elements = new Array(
 );
 
 var disableElements = new Array();
-disableElements['new'] = new Array('btnNew','btnEdit', 'btnDel', 'btnList', 'btnFirst', 'btnPrev', 'btnNext', 'btnLast');
-disableElements['edit'] = new Array('btnNew','btnEdit', 'btnDel', 'btnList');
-disableElements['del'] = new Array('btnNew','btnEdit', 'btnDel', 'btnList');
+disableElements['new'] = new Array('btnNew','btnEdit', 'btnDel', 'btnFirst', 'btnPrev', 'btnNext', 'btnLast');
+disableElements['edit'] = new Array('btnNew','btnEdit', 'btnDel', 'btnFirst', 'btnPrev', 'btnNext', 'btnLast');
+disableElements['del'] = new Array('btnNew','btnEdit', 'btnDel', 'btnFirst', 'btnPrev', 'btnNext', 'btnLast');
 
 function inicializar(){
 	btnAceptar.style.display = 'none';
@@ -35,6 +39,8 @@ function inicializar(){
 	alertError.style.display = 'none';
 	alertSuccess.style.display = 'none';
 	alertWarning.style.display = 'none';
+	alertInfo.style.display = 'none';
+	divList.style.display = 'block';
 	inputNum.value = numeros[index];
 }
 
@@ -93,12 +99,19 @@ function setEscenario(tipo){
 	if(tipo == 'edit') {
 		inputNum.removeAttribute('readonly');
 	}
+
+	if(tipo == 'del') {
+		inputNum.setAttribute('readonly','readonly');
+	}
 }
 
 function setAccion(tipo){
 	accion = tipo;
-	//console.log(tipo);
+
 	setEscenario(tipo);
+
+	//informamos;
+	setAlertInfo();
 }
 
 function cancel(){
@@ -116,11 +129,33 @@ function comprobarNumero(){
 	return res;
 }
 
+function setAlertInfo(){
+	switch(accion){
+		case 'new':
+			alertInfo.innerHTML = 'Introduce un número';
+			break;
+
+		case 'edit':
+			alertInfo.innerHTML = 'Estas modificando el número: ' + numeros[index];
+			break;
+
+		case 'del':
+			alertInfo.innerHTML = 'Estas eliminado el número: ' + numeros[index];
+			break;
+	}
+	alertInfo.style.display = 'block';
+	alertError.style.display = 'none';
+	alertSuccess.style.display = 'none';
+	alertWarning.style.display = 'none';
+	console.log(alertInfo);
+}
+
 function setAlertError(mensaje){
 	alertError.innerHTML = mensaje;
 	alertError.style.display = 'block';
 	alertSuccess.style.display = 'none';
 	alertWarning.style.display = 'none';
+	alertInfo.style.display = 'none';
 }
 
 function setAlertSuccess(mensaje){
@@ -128,15 +163,25 @@ function setAlertSuccess(mensaje){
 	alertSuccess.style.display = 'block';
 	alertError.style.display = 'none';
 	alertWarning.style.display = 'none';
+	alertInfo.style.display = 'none';
 }
 
-function setAlertWarning(){
-	document.getElementById('mensajeConfirm').innerHTML = '¿Estás seguro de eliminar el número '+ numeros[index]+' ?'
+function setAlertWarning(mensaje){
+	document.getElementById('mensajeConfirm').innerHTML = mensaje;
 	alertWarning.style.display = 'block';
 }
 
 function confirmCancel(){
 	alertWarning.style.display = 'none';
+}
+
+function checkIndex(){
+	if(index == numLength-1) {
+		index = 0;
+	}
+	if(index == 0) {
+		index = numeros.length-1;
+	}
 }
 
 function confirm(){
@@ -145,14 +190,20 @@ function confirm(){
 			var numLength = numeros.length;
 			numeros.splice(index, 1);
 			setAlertSuccess('Muy bien, número eliminado correctamente');
-			if(index == numLength-1) {
-				index = 0;
-			}
-			if(index == 0) {
-				index = numeros.length-1;
-			}
+			checkIndex();
 			inputNum.value = numeros[index];
 			list();
+			break;
+
+		case 'edit':
+			if(!isNaN(inputNum.value)) {
+				numeros[index] = parseInt(inputNum.value);
+				setAlertSuccess('Muy bien, número modificado correctamente');
+				list();
+			} else {
+				// alert no es numero
+				setAlertError('Hay que introducir un número');
+			}
 			break;
 	}
 }
@@ -173,18 +224,12 @@ function aceptar(){
 			}
 			break;
 		case 'edit':
-			if(!isNaN(inputNum.value)) {
-				numeros[index] = parseInt(inputNum.value);
-				setAlertSuccess('Muy bien, número modificado correctamente');
-				list();
-			} else {
-				// alert no es numero
-				setAlertError('Hay que introducir un número');
-			}
+			//pedir confirmación
+			setAlertWarning('¿Estás seguro de modificar el número '+ numeros[index]+' ?');
 			break;
 		case 'del':
 			//pedir confirmación
-			setAlertWarning();
+			setAlertWarning('¿Estás seguro de eliminar el número '+ numeros[index]+' ?');
 			break;
 	}
 }
@@ -209,7 +254,21 @@ function compareDesc(first, second) {
 
 function list(){
 	var cadena = '';
-	var numOrdAsc = numeros;
+
+	numeros.forEach((value,index,ar)=>{
+		numOrdAsc.push(value);
+		numOrdDesc.push(value);
+		if(index == ar.length-1) {
+			cadena += value;
+		} else {
+			cadena += value + ', ';
+		}
+	});
+
+	divListaNumeros.innerHTML = cadena;
+
+	cadena = '';
+
 	numOrdAsc.sort(compareAsc);
 	numOrdAsc.forEach((value,index,ar)=>{
 		if(index == ar.length-1) {
@@ -221,9 +280,9 @@ function list(){
 
 	divListaOrdenadaAsc.innerHTML = cadena;
 
-	numOrdAsc.sort(compareDesc);
+	numOrdDesc.sort(compareDesc);
 	cadena = '';
-	numOrdAsc.forEach((value,index,ar)=>{
+	numOrdDesc.forEach((value,index,ar)=>{
 		if(index == ar.length-1) {
 			cadena += value;
 		} else {
@@ -231,6 +290,33 @@ function list(){
 		}
 	});
 	divListaOrdenadaDesc.innerHTML = cadena;
+}
+
+function verList(){
+	if( (typeof(myWindow) == 'undefined') || (myWindow.closed) ) {
+		myWindow= window.open("","Nueva Ventana","width=500,height=400,top=100,left=100");
+
+		// Abre la corriente de datos del documento para escribir
+		myWindow.document.open();
+
+		// Crea el documento
+		myWindow.document.write("<html><head><title>Números Ordenados</title></head><body>");
+		myWindow.document.write("<div style=\"text-align:center;\">");
+		myWindow.document.write("<h1>Nuestros números</h1>");
+		myWindow.document.write("<table border=\"1\" style=\"margin: 0 auto;\" >");
+		myWindow.document.write("<tr><th>Num.</th><th>Ord. Asc</th><th>Ord. Desc</th></tr>");
+		for (var i = numeros.length - 1; i >= 0; i--) {
+			myWindow.document.write("<tr><td>"+numeros[i]+"</td><td>"+numOrdAsc[i]+"</td><td>"+numOrdDesc[i]+"</td></tr>");
+		}
+		myWindow.document.write("</table>");
+		myWindow.document.write('<p><button onClick="window.close()">Cerrar ventana</button>');
+		myWindow.document.write("</div></body></html>");
+
+		// cierra la corriente de datos del documento 
+		myWindow.document.close();
+	} else {
+		myWindow.focus();
+	}
 }
 
 function toggleList(){
@@ -244,4 +330,4 @@ function toggleList(){
 //al iniciar la página
 inicializar();
 list();
-divList.style.display = 'block';
+
